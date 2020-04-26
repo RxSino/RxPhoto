199ab11a3aab878e60ad6a6213840b625d1744e2
chenchen <905588195@qq.com>
2020-04-26 10:32:07 +0800

    fix

--- rxphoto/src/main/java/sino/android/rxphoto/RxPhotos.java
+++ rxphoto/src/main/java/sino/android/rxphoto/RxPhotos.java
@@ -1,8 +1,8 @@
 package sino.android.rxphoto;
 
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentActivity;
-import androidx.fragment.app.FragmentManager;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
 
 import io.reactivex.Observable;
 import io.reactivex.ObservableSource;
